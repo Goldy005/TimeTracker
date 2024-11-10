c3f568a5c72a7c667ddf9887cfe7646875523d1c
singhkas <shajinder.singh-kaurz-external@capgemini.com>
2024-11-10 08:46:21 +0100

    Added Relatinships-

---
+++ TimeTracker.Shared/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Shared.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime? DateUpdated { get; set; }
+
+
+    }
+}
